5453afe7ec9e11a6f84b3a020fae471f9f4c7a22
Shanthakumar <93003141+shanthakumarvenkatesh@users.noreply.github.com>
2022-04-04 18:16:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// / function add() {
+    //     console.log(5+7);
+    // }
+    // add();
+    
+    
+    // function addtwopara(a,b) {
+    //     console.log(a+b);
+    // }
+    // addtwopara(7,3);
+    
+    
+    // function parameter(first,lastname,age) {
+    // console.log(first,"is",age,"years old");
+    // }
+    // parameter("A",90);
+    
+    
+    // var aag=()=>{
+    //   console.log("hello world");
+    // }
+    // aag();
+    
+    // var x = 21;
+    // var girl = function () {
+    //     console.log(x);
+    //     var x = 20;
+    // }
+    // girl ();
+    
+    
+     
+    // var x = 21;
+    // girl ();
+    // console.log(x)
+    // function girl() {
+    //     console.log(x);
+    //     var x = 20;
+    // };
+    
+    // ans undifined 21
+    
+    // var x = 21;
+    // a();
+    // b();
+    // console.log(a);
+    // function a () {
+        
+    //    x = 20;
+    //   console.log(x);
+    // };
+    // function b () {
+        
+    //     x = 40;
+    //    console.log(x);
+    // };
+    
+    
+    // ans is 20 40 and whole function of a 
+    
+    //  var x = 21;
+    //  a();
+    //  b();
+    //  console.log(a);
+    //  a = function() {
+        
+    //     x = 20;
+    //    console.log(x);
+    //  };
+    //  b = function() {
+        
+    //      x = 40;
+    //     console.log(x);
+    //  };
